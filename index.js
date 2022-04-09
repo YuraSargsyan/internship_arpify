66ae365f116a8598b86003f85e9a49b7f25c4a44
YuraSargsyan <yura.mail.ru@inbox.ru>
2022-04-09 23:24:02 +0300

    implement_page1

--- index.js
+++ index.js
@@ -1,61 +1,46 @@
-const usersProfile = [
-    {
-        firstName: 'Yura',
-        lastName: 'Sargsyan',
-        birthDay: '06.02.1994',
-        gender: 'male'
-    }
-
-]
-
-(function load() {
-    fetch('http://intern-2022.arpify.com/init', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: userYura
-    })
-    .then(resp => resp.json())
-    .then(result => console.log(result))
-})()
-
-function handleDeleteUser(userIndex) {
-    const user = usersProfile[userIndex];
-    const firstName = user.firstName;
-    const lastName = user.lastName;
-
-    const isDelete = confirm(`Duq cankanum eq heracnel ${firstName} ${lastName} - in ?` )
-        if(isDelete) {
-            usersProfile.splice(userIndex, 1)
-            tableBody(usersProfile)
-        }
+const myData = {
+    firstName: 'Yura',
+    lastName: 'Sargsyan',
+    birthDay: '1994-02-06',
+    gender: 'male'
 }
 
+const usersData = [];
+
+
+
+function sendRequest() {
+    return fetch('http://intern-2022.arpify.com/init', {
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(myData)
+    })
+    .then(response => response.json())
+    .then(data => usersData.push(data))
+    // .then(res => console.log(usersData))
+    .then(result => tableBody(usersData))
+}
+
+
+(sendRequest)()
+
 function tableBody(data) {
-    const tBody = document.getElementById('userTable');
+    const userTable = document.getElementById('userTable');
+    userTable.innerHTML = '';
 
-    tBody.innerHTML = '';
-
-    data.forEach((user, index) => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-            <td>${index + 1}</td>
-            <td>${user.firstName}</td>
-            <td>${user.lastName}</td>
-            <td>${user.phone}</td>
-            <td>${user.gender}</td>
-            <td>
-                <button>Delete</button>
-                <button>Edit</button>
-            </td>
-        `
-        tr.onclick = (e) => {
-            if(e.target.innerHTML === 'Delete') {
-                handleDeleteUser(index)
-            }
-        }
-        tBody.appendChild(tr)
-        
+    data.forEach(usersData => {
+        usersData.forEach(user => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.birthDay}</td>
+                <td>${user.gender}</td>
+            `
+            if(myData == usersData[myData])
+            userTable.appendChild(tr)
+        }); 
     });
 }
